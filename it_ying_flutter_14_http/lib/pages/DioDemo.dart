65f354eef70e84ca1600f0cdf253eaf621acd431
phoenixfate <sm516116978@outlook.com>
2020-09-29 15:59:40 +0800

    多接口等待

--- it_ying_flutter_14_http/lib/pages/DioDemo.dart
+++ it_ying_flutter_14_http/lib/pages/DioDemo.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 import 'package:dio/dio.dart' as dio;
 
-
 class DioDemo extends StatefulWidget {
   DioDemo({Key key}) : super(key: key);
 
@@ -18,6 +17,7 @@ class _DioDemoState extends State<DioDemo> {
   void initState() {
     super.initState();
     this._getData();
+    this._getAll();
   }
 
   _getData() async {
@@ -29,16 +29,32 @@ class _DioDemoState extends State<DioDemo> {
     });
   }
 
+  _getAll() async {
+    print("----get all start--");
+    var apiUrl1 = "https://www.njrzzk.com/weixin/calcRankByScore?score=53";
+    var apiUrl2 = "https://www.njrzzk.com/weixin/calcRankByScore?score=53";
+    List<Future> temp = List();
+    temp.add(dio.Dio().get(apiUrl1));
+    temp.add(dio.Dio().get(apiUrl2));
+    List result = await Future.wait(temp);
+    print(result);
+    print("----get all end--");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("dio demo"),
         ),
-        body: this._list.length>0?ListView.builder(
-          itemCount: this._list.length,
-          itemBuilder: (context,index){
-            return ListTile(title: Text(this._list[index]["title"]),);
-          }):Text("加载中..."));
+        body: this._list.length > 0
+            ? ListView.builder(
+                itemCount: this._list.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(this._list[index]["title"]),
+                  );
+                })
+            : Text("加载中..."));
   }
 }
